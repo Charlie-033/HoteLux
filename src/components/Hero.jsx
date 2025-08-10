8c49df19e7022898a1f54ae8a4c6cdda389e327f
Charlie-033 <ahmedeleus1@gmail.com>
2025-08-10 17:11:08 +0600

    Set a relevant and high quality image on hero section slider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,19 +7,19 @@ const Hero = () => {
 
   const images = [
     {
-      src: "https://i.ibb.co/cKMXLs4y/banner-1.webp",
+      src: "https://i.ibb.co.com/wh7mTYKL/1978.jpg",
       title: "Secluded Paradise in the Seychelles",
       description:
         "Escape to an idyllic retreat overlooking the crystal-clear waters of the Seychelles. This luxurious resort, possibly an Aman property, offers unparalleled serenity with private infinity pools nestled amidst lush tropical foliage. Palm trees sway gently against a backdrop of the azure ocean, inviting guests to unwind and immerse themselves in the tranquility of this breathtaking island paradise.",
     },
     {
-      src: "https://i.ibb.co/0ymFZXXL/banner-2.jpg",
+      src: "https://i.ibb.co.com/Swkr80Hd/4908.jpg",
       title: "Oasis of Luxury: Four Seasons Poolside Retreat",
       description:
         "Indulge in sophisticated relaxation at this exquisite Four Seasons resort pool, possibly in Surfside, Florida, or Chileno Bay. Lined with elegant cabanas and plush lounge chairs, the serene poolside ambiance invites guests to bask in the sun and unwind. Lush landscaping and the clear blue water create an ideal setting for a luxurious escape, offering both comfort and scenic beauty.",
     },
     {
-      src: "https://i.ibb.co/Fkw5JQNs/banner-3.webp",
+      src: "https://i.ibb.co.com/9HyRNXzt/2151961782.jpg",
       title: "Dubai Grandeur: Luxury Room with City Views",
       description:
         "Experience the epitome of urban luxury in this spacious hotel room, likely at The St. Regis Downtown Dubai. Floor-to-ceiling windows offer breathtaking panoramic views of the iconic Dubai skyline, including the majestic Burj Khalifa. The elegant decor, featuring plush furnishings and modern design, creates a sophisticated and comfortable retreat perfect for enjoying the vibrant cityscape.",
@@ -50,7 +50,7 @@ const Hero = () => {
 
         {/* Static text content on top */}
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-30 px-6">
-          <h1 className="text-3xl md:text-5xl lg:text-7xl font-semibold text-white drop-shadow-lg">
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-semibold text-[#f1f1f1] drop-shadow-lg">
             Explore Your <br /> Luxury
           </h1>
           <div className="mt-4 bg-black/30 px-6 py-4 rounded-lg max-w-xl">
